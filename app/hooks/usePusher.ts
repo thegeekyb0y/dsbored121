e5beaa0e519cf6664c553f5f2d35e5a6231c1b52
Aditya Tiwari <thegeekyboy.in@gmail.com>
2025-11-09 00:53:32 +0530

    ...

--- app/hooks/usePusher.ts
+++ app/hooks/usePusher.ts
@@ -4,22 +4,24 @@ import { useEffect, useState } from "react";
 import PusherClient, { Channel } from "pusher-js";
 
 export function usePusher(channelName: string) {
-  const [pusher, setPusher] = useState<PusherClient | null>(null);
   const [channel, setChannel] = useState<Channel | null>(null);
 
   useEffect(() => {
-    const pusherClient = new PusherClient(process.env.NEXT_PUBLIC_PUSHER_KEY!, {
+    const pusher = new PusherClient(process.env.NEXT_PUBLIC_PUSHER_KEY!, {
       cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
     });
 
-    const pusherChannel = pusherClient.subscribe(channelName);
-    setPusher(pusherClient);
-    setChannel(pusherChannel);
+    const ch = pusher.subscribe(channelName);
+
+    // Defer setState to next tick
+    const timer = setTimeout(() => setChannel(ch), 0);
 
     return () => {
-      pusherChannel.unsubscribe();
-      pusherClient.disconnect();
+      clearTimeout(timer);
+      ch.unsubscribe();
+      pusher.disconnect();
     };
   }, [channelName]);
-  return { pusher, channel };
+
+  return { channel };
 }
